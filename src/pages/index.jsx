4c6a2ef19ef628b390b93b8a30c2fc6cc2513067
Somiona <somionat@gmail.com>
2019-10-22 13:38:38 -0400

    ts

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/Header"
+
+export default () => (
+  <div>
+    <Header headerText={"Header"}/>
+    <div>Hello gatsby!</div>
+  </div>
+)
