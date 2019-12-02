f45406917a1df651af6913d52249c237036fd457
SomioNaArashi <somionat@gmail.com>
2019-12-02 00:12:52 -0500

    initial publish

--- src/styles/components/header.module.scss.d.ts
+++
@@ -1,1 +0,0 @@
-export const scssHeader: string;
---
+++ styles-5245db8afba00e5be720.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"5bN+":function(o,s,e){o.exports={scssFooter:"footer-module--scssFooter--3u_UQ"}}}]);
+//# sourceMappingURL=styles-5245db8afba00e5be720.js.map
